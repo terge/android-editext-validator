e11df21b86b86d1792a54735a824337b3dd15bb9
terge <i.terge.me@gmail.com>
2016-12-08 11:05:21 +0800

    implement helper

--- library/src/main/java/vip/xioix/aev/library/ValidatorHelper.java
+++ library/src/main/java/vip/xioix/aev/library/ValidatorHelper.java
@@ -1,8 +1,57 @@
 package vip.xioix.aev.library;
 
+import android.widget.EditText;
+
+import vip.xioix.aev.library.validator.OrValidator;
+import vip.xioix.aev.library.validator.Validator;
+
 /**
  * Created by terge on 16-12-8.
  */
 
 public class ValidatorHelper {
+
+    private EditText mEt;
+    private ValidatorHelper(EditText editText){
+        mEt = editText;
+    }
+
+    public static ValidatorHelper from(EditText editText){
+        return new ValidatorHelper(editText);
+    }
+
+    private Validator mValidator;
+    public ValidatorHelper set(Validator validator){
+        mValidator = validator;
+        return this;
+    }
+
+    public ValidatorHelper or(Validator validator){
+        if(mValidator == null){
+            mValidator = validator;
+        }else {
+            mValidator = new OrValidator(mValidator,validator);
+        }
+
+        return this;
+    }
+
+    private String fixText = null;
+    private boolean isNeedFix = false;
+    public ValidatorHelper fix(String fix){
+        isNeedFix = true;
+        fixText = fix;
+        return this;
+    }
+
+
+    public boolean check(boolean showError){
+        boolean result =  mValidator.isValid(mEt);
+        if(showError){
+            mEt.requestFocus();
+            mEt.setError(mValidator.getErrorMessage());
+            if(isNeedFix)mEt.setText(fixText);
+        }
+        return result;
+    }
 }
--- library/src/main/java/vip/xioix/aev/library/validator/OrValidator.java
+++ library/src/main/java/vip/xioix/aev/library/validator/OrValidator.java
@@ -12,6 +12,11 @@ public class OrValidator extends MultiValidator {
 
     public OrValidator(String message, Validator... validators) {
         super(message, validators);
+        if(message == null) errorMessage = "输入有误";
+    }
+
+    public OrValidator(Validator... validators) {
+        this(null,validators);
     }
 
     public boolean isValid(EditText et) {
